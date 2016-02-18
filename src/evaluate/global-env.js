8541900fc0e7b2cbaa9017bfe397f94ee185e09d
Ethan Resnick <ethan.resnick@gmail.com>
2016-02-18 05:56:40 -0500

    Better = function

--- src/evaluate/global-env.js
+++ src/evaluate/global-env.js
@@ -7,10 +7,9 @@ module.exports =  {
     return Array.from(arguments).reduce(((acc, it) => acc + it), 0);
   },
 
-  // This doesn't yet account for Immutable.js types.
   "=": function(/* ... args */) {
     const args = Array.from(arguments);
-    return args.every(it => it === args[0]);
+    return args.every((it) => Immutable.is(it, args[0]));
   },
 
   "symbol?": function(val) {
